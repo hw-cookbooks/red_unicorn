ad658ba4fbf767f7b19ccfc5824002845e2e3090
Sean Porter <portertech@gmail.com>
2014-01-28 10:40:44 -0800

    minor version bump; zero downtime restart, before/after fork hooks, ruby fixes

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'chrisroberts.code@gmail.com'
 license 'Apache 2.0'
 description 'Installs and configures red_unicorn'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.2.0'
 
 
 # NOTE: Currently forcibly configured via bluepill. Options for
